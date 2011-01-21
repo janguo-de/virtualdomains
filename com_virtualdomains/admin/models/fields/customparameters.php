b924fffffe86fd6783e9dc6468adc39f447db309
romacron <romacron@janguo.de>
2011-01-21 10:55:29 +0000

    PHP Doc file header changed to Michael JText::_('') for titles "Pattern" now same as the showing Patterns (only synchron niced)
    git-svn-id: svn://tools.janguo.de/virtualdomains/trunk@33 43e91662-23d2-4b0b-b03f-4c4df6fcf87b

--- com_virtualdomains/admin/models/fields/customparameters.php
+++ com_virtualdomains/admin/models/fields/customparameters.php
@@ -2,12 +2,12 @@
  * @date		$Date: 2011-01-09 14:15:41 +0100 (So, 09 Jan 2011) $
  * @revision    $Rev: 22 $
  * @id 			$Id: costomparameters.php 22 2011-01-09 13:15:41Z romacron $
- * @version		romacron com_virtualdomains $
+ * @version		!j 1.6 Series com_virtualdomains $
  * @package		com_virtualdomains Webmaster
  * @copyright	Copyright © 2010 - All rights reserved.
- * @author		romacron
- * @authorMail	info@romacron.de
- * @website		http://www.romacron.de
+ * @author		michael liebler
+ * @authorMail	michael-liebler@janguo.de
+ * @website		http://www.janguo.de
  *
  * @description Shows The Form with Costom Parameter KEYs
  * This is the KEY Pattern
@@ -28,28 +28,40 @@ class JFormFieldCustomparameters extends JFormField
      */
     public $type = 'Customparameters';
 
-	protected function getInput()
+    protected function getInput()
     {
-            $doc = &JFactory::getDocument();
+        $doc = &JFactory::getDocument();
 
+        /**
+         * file doesn't exists 
+         * @notice  js-pattern will get from 1.5. params.js (if its possible)
+         */
         $doc->addScript( JURI::base() . 'components/com_virtualdomains/assets/js/params16.js' );
 
         $cParams = &JComponentHelper::getParams( 'com_virtualdomains' );
 
         $k = $cParams->get( 'costomParameterKey' );
 
-        $paramsHtml = "<div style='clear:both'  id=\"existing_params\">";
-        if ( $k ) {
-        	
-            if ( !is_array( $k ) ) {
-                   $paramsHtml .= '<fieldset class="text"><input class="keyname" name="jform[costomParameterKey][]" value="' . $k . '" type="text">
-                <a class="jgrid"  href="javascript:void(null);" title="' . JText::_( 'REMOVE' ) . '" onclick="vd.remove_key(this);"><span class="state unpublish"/><span class="text">Remove</span></span></a></fieldset>';
-                
-            } else {
-            	
-                foreach ( $k as $key ) {
+        $paramsHtml = "<div style='clear:both' id=\"existing_params\">";
+        if ( $k )
+        {
+
+            if ( !is_array( $k ) )
+            {
+                $paramsHtml .= '<fieldset class="text"><input class="keyname" name="jform[costomParameterKey][]" value="' . $k . '" type="text">
+                <a class="jgrid"  href="javascript:void(null);" title="' . JText::_( 'REMOVE' ) .
+                    '" onclick="vd.remove_key(this);"><span class="state unpublish"/><span class="text">' . JText::_( 'REMOVE' ) .
+                    '</span></span></a></fieldset>';
+
+            } else
+            {
+
+                foreach ( $k as $key )
+                {
                     $paramsHtml .= '<fieldset class="text"><input name="jform[costomParameterKey][]" value="' . $key . '" type="text">
-                <a class="jgrid"  href="javascript:void(null);" title="' . JText::_( 'REMOVE' ) . '" onclick="vd.remove_key(this);"><span class="state unpublish"/><span class="text">Remove</span></span></a></fieldset>';
+                <a class="jgrid"  href="javascript:void(null);" title="' . JText::_( 'REMOVE' ) .
+                        '" onclick="vd.remove_key(this);"><span class="state unpublish"/><span class="text">' . JText::_( 'REMOVE' ) .
+                        '</span></span></a></fieldset>';
                 }
             }
         }
@@ -57,10 +69,9 @@ class JFormFieldCustomparameters extends JFormField
 
         $paramsHtml .= "<a href=\"javascript:void(null);\" onclick=\"vd.add_key();\">" . JText::_( 'ADD PARAMS FIELD' ) . "</a>";
         $paramsHtml .= '<p id="vd_key_pattern">
-              <div style="display:none" id="remove_pattern"><a href="javascript:void(null);" title="' . JText::_( 'REMOVE' ) .
-            '" onclick="vd.remove_key(this);">
-        <img src="images/publish_x.png"/>
-        </a></div>';
+              <div style="display:none" id="remove_pattern"><a class="jgrid"  href="javascript:void(null);" title="' . JText::_( 'REMOVE' ) .
+            '" onclick="vd.remove_key(this);"><span class="state unpublish"/><span class="text">' . JText::_( 'REMOVE' ) . '</span></span></a></div>';
+
         return $paramsHtml;
     }
 
