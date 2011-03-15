906f40718a8c074dbe658ce56e13a1c0f4695ff1
michel <michael-liebler@janguo.de>
2011-03-15 22:07:59 +0000

    Falsche Bilder in eng.  Hilfe - Roman ins about
    git-svn-id: svn://tools.janguo.de/virtualdomains/trunk@60 43e91662-23d2-4b0b-b03f-4c4df6fcf87b

--- com_virtualdomains/admin/views/about/tmpl/default.php
+++ com_virtualdomains/admin/views/about/tmpl/default.php
@@ -1,24 +1,28 @@
 <?php
 defined('_JEXEC') or die( 'Restricted access' );
 	JToolBarHelper::title(   JText::_( 'About' ), 'generic.png' );
-	JToolBarHelper::help( 'virtualdomain', 1 ); 
+	 VirtualdomainsHelper::helpIcon('About');
 ?>
 <fieldset class="adminform">
 <legend>About</legend>
 <table class="admintable" width="100%">
   <tbody>
   <tr class="row0">
-    <td class="key">Author</td>
-    <td>Michael Liebler</td>
+    <td class="key">Authors</td>
+    <td><a href="http://janguo.de">Michael Liebler</a>, <a href="http://romacron.de">Roman Teske</a></td>
   </tr>
   <tr class="row1">
-    <td class="key">Website</td>
-    <td><a href="http://www.janguo.de">www.janguo.de</a></td>
+    <td class="key">Project Website</td>
+    <td><a href="http://redmine.janguo.de/projects/vd-main">redmine.janguo.de</a></td>
   </tr>
   <tr>
     <td class="key">Support Forum</td>
     <td><a href="http://forum.janguo.de">forum.janguo.de</a></td>
-  </tr>  
+  </tr>
+<tr>
+    <td class="key">Wiki</td>
+    <td><a href="http://redmine.janguo.de/projects/vd-main/wiki">Wiki</a></td>
+  </tr>      
   </tbody>
 </table>
 </fieldset>
