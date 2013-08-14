659560e5a888e5322f9652bb28c3492c3deb7b0d
michel <michael-liebler@janguo.de>
2013-08-14 16:58:03 +0000

    Set header for crossdomain json
    git-svn-id: svn://tools.janguo.de/virtualdomains/trunk@157 43e91662-23d2-4b0b-b03f-4c4df6fcf87b

--- com_virtualdomains/site/virtualdomains.php
+++ com_virtualdomains/site/virtualdomains.php
@@ -5,6 +5,7 @@ $data = json_encode(array('hostname'=>$host));
 ob_clean();
 header('Cache-Control: no-cache, must-revalidate');
 header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
-header('Content-type: application/json');
+header('content-type: application/json; charset=utf-8');
+header("access-control-allow-origin: *");
 print json_encode($data);
 exit; 
